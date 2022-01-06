8caa06678f3106b49e8bf14e4316dd8917d8c50d
sam.gray121@gmail.com <Mrtwister123!>
2022-01-05 19:01:18 -0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,30 @@
+package com.company;
+
+import java.util.Scanner;
+import java.util.function.DoubleBinaryOperator;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner inputReader = new Scanner (System.in);
+
+        String yourName;
+        String yourQuest;
+        double velocityOfSwallow;
+
+        System.out.print("What is your name?? ");
+        yourName = inputReader .nextLine();
+
+        System.out.print("What is your quest?! ");
+        yourQuest = inputReader.nextLine();
+
+        System.out.print("What is the airspeed velocity of an unladen swallow?!?! ");
+        velocityOfSwallow = inputReader .nextDouble();
+
+        System.out.println("How do you know " + velocityOfSwallow + " is correct," + yourName + ",");
+        System.out.println("when you didn't even know if the swallow was African or European!");
+        System.out.println("Maybe skip answering things about birds and instead go " +yourQuest);
+
+    }
+}
